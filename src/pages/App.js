082693fe8f5d1ba45713c92531d25238277c7bc7
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-08-07 13:51:25 -0500

    Created components and added style

---
+++ src/pages/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.css';
+import Navbar from '../components/navbar';
+import Routes from '../routes/routes';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/products.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Products extends React.PureComponent {
+  render() {
+    return (
+      <div>Products Work!</div>
+    )
+  }
+}
+
+export default Products;
